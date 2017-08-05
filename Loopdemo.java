a55f2e6cc525e33ce41f88a9fb932f625de88853
altariwing <altariwing@gmail.com>
2017-08-05 18:27:24 +0800

    0627

---
+++ Loopdemo.java
@@ -0,0 +1,24 @@
+package com.altariwing.loopdemo;
+
+public class Loopdemo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Accumulator accumulator=new Accumulator();
+        
+        System.out.println(accumulator.accumulateToUsingForLoop(100));
+        System.out.println(accumulator.accumulateToUsingWhileLoop(100));
+        System.out.println(accumulator.accumulateToUsingDoWhileLoop(100));
+       
+        System.out.println(accumulator.accumulateToUsingForLoop(0));
+        System.out.println(accumulator.accumulateToUsingWhileLoop(0));
+        System.out.println(accumulator.accumulateToUsingDoWhileLoop(0));
+        
+        System.out.println(accumulator.returnEndIndexForTotal(50));
+        System.out.println(accumulator.returnEndIndexForTotalUsingInfinityLoop(50));
+        System.out.println(accumulator.returnEndIndexForTotal(5050));
+        System.out.println(accumulator.returnEndIndexForTotalUsingInfinityLoop(5050));
+
+    }
+
+}
